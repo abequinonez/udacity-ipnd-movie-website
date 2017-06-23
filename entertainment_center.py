37062ddb23f1fec07e820b45c2ccf3b488ca26fb
Abraham Quinonez <abequinonez@users.noreply.github.com>
2017-06-22 21:50:07 -0700

    Updated image links

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,7 +3,7 @@ import media
 
 # The following 6 movies are instances of the Movie class
 blade_runner = media.Movie("Blade Runner",
-							"http://www.impawards.com/1982/posters/blade_runner_xlg.jpg",
+							"https://madspoon.net/images/filmovi/blade-runner.jpeg",
 							"https://www.youtube.com/watch?v=W_9rhPDLHWk",
 							1982,
 							"Ridley Scott",
@@ -11,7 +11,7 @@ blade_runner = media.Movie("Blade Runner",
 							media.Movie.VALID_RATINGS[3])
 
 robocop = media.Movie("Robocop",
-						"http://www.theterminatorfans.com/wp-content/uploads/2013/09/RoboCop-1987-Poster.jpg",
+						"https://www.theterminatorfans.com/wp-content/uploads/2013/09/RoboCop-1987-Poster.jpg",
 						"https://www.youtube.com/watch?v=qaNRzjGsOeA",
 						1987,
 						"Paul Verhoeven",
@@ -19,7 +19,7 @@ robocop = media.Movie("Robocop",
 						media.Movie.VALID_RATINGS[3])
 
 kill_bill = media.Movie("Kill Bill",
-						"http://s2.thingpic.com/images/xr/yJbD1JBhmpbD5AK2mW6Aq95U.jpeg",
+						"http://img.csfd.cz/files/images/user/profile/159/451/159451641_729a41.jpg",
 						"https://www.youtube.com/watch?v=7kSuas6mRpk",
 						2003,
 						"Quentin Tarantino",
@@ -27,7 +27,7 @@ kill_bill = media.Movie("Kill Bill",
 						media.Movie.VALID_RATINGS[3])
 
 ghost_in_the_shell = media.Movie("Ghost in the Shell",
-								"http://68.media.tumblr.com/bbf0183b4e3d85f806930d0dbc4dc756/tumblr_n9c3rc55Bp1qbluruo2_r1_1280.jpg",
+								"https://68.media.tumblr.com/bbf0183b4e3d85f806930d0dbc4dc756/tumblr_n9c3rc55Bp1qbluruo2_r1_1280.jpg",
 								"https://www.youtube.com/watch?v=Dfqnbp8AJ9U",
 								1995,
 								"Mamoru Oshii",
@@ -35,7 +35,7 @@ ghost_in_the_shell = media.Movie("Ghost in the Shell",
 								media.Movie.VALID_RATINGS[3])
 
 akira = media.Movie("Akira",
-					"http://i443.photobucket.com/albums/qq155/ACNRVP/Kaneda/Kaneda%20Out%20of%20The%20Box/akiraMoviePoster1.jpg",
+					"https://i443.photobucket.com/albums/qq155/ACNRVP/Kaneda/Kaneda%20Out%20of%20The%20Box/akiraMoviePoster1.jpg",
 					"https://www.youtube.com/watch?v=pC6Qk5XxGIY",
 					1988,
 					"Katsuhiro Otomo",
@@ -43,7 +43,7 @@ akira = media.Movie("Akira",
 					media.Movie.VALID_RATINGS[3])
 
 spirited_away = media.Movie("Spirited Away",
-							"http://www.impawards.com/2002/posters/spirited_away.jpg",
+							"https://upontheshelfreviews.files.wordpress.com/2016/04/600full-spirited-away-poster.jpg",
 							"https://www.youtube.com/watch?v=ByXuk9QqQkk",
 							2001,
 							"Hayao Miyazaki",
